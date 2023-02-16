2378885ffcc40f053ec69ff0e04f180c1febd310
jeff <jeffreyjohn102@gmail.com>
2023-02-16 18:51:33 -0500

    Add comments and constant variables to CLI file

--- todo-list/src/main/java/org/todolist/TodoListCLI.java
+++ todo-list/src/main/java/org/todolist/TodoListCLI.java
@@ -6,9 +6,14 @@ import org.todolist.main.TodoList;
 
 public class TodoListCLI {
 
+    private final String MAIN_MENU_LIST_PROJECTS = "List projects";
+    private final String MAIN_MENU_CREATE_PROJECT = "Create project";
+    private final String MAIN_MENU_EXIT = "Exit";
+
     Scanner userInput = new Scanner(System.in);
     TodoList tl = new TodoList();
 
+    // Main function to run program
     public void run() {
 
         boolean isRunning = true;
@@ -19,6 +24,7 @@ public class TodoListCLI {
 
         while (isRunning) {
 
+            // Run initially when program runs and project list is empty
             if (tl.getPL().getProjectList().isEmpty()) {
                 System.out.print("It looks like you don't have any projects!\n" +
                         "Enter [N] to create one now\n" +
@@ -26,6 +32,7 @@ public class TodoListCLI {
 
                 String newInput = userInput.nextLine().toUpperCase();
 
+                // Verify user input based on options
                 while (!newInput.matches("N|Q")) {
                     System.out.print("\nNot a valid option, please try again: ");
                     newInput = userInput.nextLine().toUpperCase();
@@ -33,18 +40,31 @@ public class TodoListCLI {
 
                 switch (newInput) {
                     case "N":
-
+                        // Prompt user for project creation
+                        newProjectPrompt();
                         isRunning = false;
                         break;
+
                     case "Q":
+                        // Exit program
                         System.out.println("Goodbye...");
                         isRunning = false;
                         break;
                 }
 
             }
+            else {
+                displayMainMenu();
+            }
 
         }
+
+    }
+
+    public void displayMainMenu() {
+
+        System.out.println("What would you like to do? ");
+
 
     }
 
@@ -53,7 +73,19 @@ public class TodoListCLI {
         System.out.print("Enter new project title: ");
 
         String newProjectTitle = userInput.nextLine();
-        tl.createNewProject(newProjectTitle);
+
+        while (!newProjectTitle.matches("[a-zA-Z0-9 ]+")) {
+            System.out.println("Not a valid title, try again");
+            newProjectTitle = userInput.nextLine();
+        }
+
+        System.out.println("Success");
+
+//        tl.createNewProject(newProjectTitle);
+
+    }
+
+    public void listProjects() {
 
     }
 
